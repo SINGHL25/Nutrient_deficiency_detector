5a2551a912f48cc74835182cd0ce30afcac47c80
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-12 07:41:03 +1000

    Update MainActivity.java

--- nutrient-detector-android-wrapper/app/src/main/java/com/example/nutrientdetectorapp/MainActivity.java
+++ nutrient-detector-android-wrapper/app/src/main/java/com/example/nutrientdetectorapp/MainActivity.java
@@ -1,1 +1,21 @@
+package com.example.nutrientdetectorapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        web = new WebView(this);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.setWebViewClient(new WebViewClient());
+        web.loadUrl("http://127.0.0.1:8501");  // Replace with local IP or ngrok public URL
+        setContentView(web);
+    }
+}
 
