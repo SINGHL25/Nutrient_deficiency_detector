c897358671cdf698ec0d195239826516a95c646a
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-12 07:47:41 +1000

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,6 @@
+# This file marks the directory as a Python package.
+# You can leave it empty or use it to import key modules for easier access.
+
+from .image_utils import process_image_and_predict
+from .translator import translate_text
 
